69ddbb3d8f7ba977c309eb2e8c228cf8dbe03b6f
ndry <andriy.kashchynets@gmail.com>
2017-07-27 21:15:39 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Phaser + typescript template
+
+Based on https://github.com/fairydhwen/PhaserTypeScriptTemplate
---
+++ app.ts/game.ts
@@ -0,0 +1,24 @@
+﻿import {Boot} from "./gameStates/boot";
+import {Preloader} from "./gameStates/preloader";
+import {MainMenu} from "./gameStates/mainMenu";
+import {Level01} from "./gameStates/level01";
+
+
+
+export class GameEngine extends Phaser.Game {
+
+    constructor() {
+        super(1024, 800, Phaser.AUTO, 'content', null);
+
+        this.state.add('Boot', Boot, false);
+        this.state.add('Preloader', Preloader, false);
+        this.state.add('MainMenu', MainMenu, false);
+        this.state.add('Level01', Level01, false);
+
+        this.state.start('Boot');
+
+        
+    }
+}
+
+const game = new GameEngine();
